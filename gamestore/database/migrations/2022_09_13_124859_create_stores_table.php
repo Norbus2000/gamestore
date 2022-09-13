1dfdd85db457ab4d6674e9510acb02eb143c8d00
Norbus2000 <odornorbert.on@gmail.com>
2022-09-13 15:16:11 +0200

    store hozzaadva

--- gamestore/database/migrations/2022_09_13_124859_create_stores_table.php
+++ gamestore/database/migrations/2022_09_13_124859_create_stores_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\store;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,11 @@ return new class extends Migration
             $table->foreignId('games_id')->references('games_id')->on('games');
             $table->timestamps();
         });
+
+        store::create(['user_id'=>'1','games_id'=>'1']);
+        store::create(['user_id'=>'2','games_id'=>'2']);
+        store::create(['user_id'=>'3','games_id'=>'3']);
+
     }
 
     /**
